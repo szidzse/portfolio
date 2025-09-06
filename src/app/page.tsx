dc831e536704a242c6f68b6e0c11c9f47f91b610
Sós Gellért <gellertsos@gmail.com>
2025-09-06 16:04:04 +0200

    put navbar into layout to display on all page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { ThemeDropdown } from "@/components/ThemeDropdown";
-
 export default function Home() {
   return (
     <article>
-      <ThemeDropdown />
       <h1 className="font-medium"> hi Gellért here</h1>
     </article>
   );
--- src/components/ChatToggle.tsx
+++ src/components/ChatToggle.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Bot, BotOff } from "lucide-react";
 
 const ChatToggle = () => {
   // TODO: implement chatbot
 
   return (
     <Button size="icon" variant="ghost">
-      *chatbot icon*
+      <Bot />
     </Button>
   );
 };
